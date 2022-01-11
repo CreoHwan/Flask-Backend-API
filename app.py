565833378e4032a173a52a0c17422664baf84db2
JungHwan Lee <92836045+CreoHwan@users.noreply.github.com>
2022-01-11 14:55:36 +0900

    Link DB and API

--- app.py
+++ app.py
@@ -1,8 +1,118 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask.json import JSONEncoder
+from sqlalchemy import create_engine, text
 
-app = Flask(__name__)
+class CustomJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, set):    return list(obj)
+
+        return JSONEncoder.default(self,obj)
+app.json_encoder = CustomJSONEncoder
+
+def create_app(test_config = None):
+    app = Flask(__name__)
+
+    if test_config is None:
+        app.config.from_pyfile("config.py")
+    else:
+        app.config.update(test_config)
+
+    database = create_engine(app.config['DB_URL'], encoding = 'utf-8', max_overflow = 0)
+    app.database = database
+
+    return app
+
 
 @app.route("/ping", methods=['GET'])
-
 def ping():
-    return "pong junghwan"
+    return "pong"
+
+@app.route("/user-now", methods=['GET'])
+def user_now():
+    return jsonify(app.users)
+
+@app.route("/tweet-now", methods=['GET'])
+def tweet_now():
+    return jsonify(app.tweets)
+
+app.users = {}
+app.id_count = 1
+@app.route("/sign-up", methods=['POST'])
+def sing_up():
+    new_user = request.json
+    new_user["id"] = app.id_count
+    app.users[app.id_count] = new_user
+    app.id_count += 1
+
+    return jsonify(new_user)
+
+app.tweets = []
+@app.route("/tweet", methods=['POST'])
+def tweet():
+    payload = request.json
+    user_id = int(payload['id'])
+    tweet = payload['tweet']
+
+    if user_id not in app.users:
+        return 'We don\'t have the user', 400
+
+    if len(tweet) > 300:
+        return 'You exceed the maximum', 400
+
+    app.tweets.append({
+        'user_id' : user_id,
+        'tweet' : tweet
+        })
+
+    return '', 200
+
+
+@app.route("/follow", methods=['POST'])
+def follow():
+    payload = request.json
+    user_id = int(payload['id'])
+    user_id_to_follow = int(payload['follow'])
+    
+    if user_id not in app.users or user_id_to_follow not in app.users:
+        return 'We don\'t have the user', 400
+
+    user = app.users[user_id]
+    user.setdefault('follow',set()).add(user_id_to_follow)
+
+    return jsonify(user)
+
+            
+@app.route("/unfollow", methods=['POST'])
+def unfollow():
+    payload = request.json
+    user_id = int(payload['id'])
+    user_id_to_unfollow = int(payload['unfollow'])
+
+    if user_id not in app.users or user_id_to_unfollow not in app.users:
+        return 'We don\'t have the user', 400
+
+    user = app.users[user_id]
+    user.setdefault('follow',set()).discard(user_id_to_unfollow)
+
+    return jsonify(user)
+    
+@app.route("/timeline/<int:user_id>", methods=['GET'])
+def timeline(user_id):
+    if user_id not in app.users:
+        return 'We don\'t have the user', 400
+
+    follow_list = app.users[user_id].get('follow',set())
+    follow_list.add(user_id)
+    timeline = [tweet for tweet in app.tweets if tweet['user_id'] in follow_list]
+
+    return jsonify({
+        'user_id' : user_id,
+        'timeline' : timeline
+        })
+
+
+
+
+
+
+
---
+++ test.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine, text
+
+db = {
+        'user' : 'root',
+        'password' : 'test1234',
+        'host' : 'localhost',
+        'port' : 3306,
+        'database' : 'miniter'
+}
+
+db_url = f"mysql+mysqlconnector://{username}:{password}@{host}:{port}/{database}?charset=utf8"
+db = create_engine(db_url, encoding = 'utf-8', max_overflow = 0)
+
+params = {'name' : '이정환'}
+rows = db.execute(text("SELECT * FROM users WHERE name = :name"),params).fetchall()
+
+for row in rows:
+    print(f"name : {row['name']}")
+    print(f"email : {row['email']}")
